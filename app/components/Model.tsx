18c3894099e597abff32b6ccc465d195264d83fa
tienld235 <leductien1997123@gmail.com>
2023-05-28 22:22:25 +0700

    implement

---
+++ app/components/Model.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import "@google/model-viewer";
+
+const Model = () => {
+  return (
+    <div id="card">
+      <model-viewer
+        autoplay
+        animation-name="bite"
+        src="/Rampaging T-Rex.glb"
+        ios-src=""
+        poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
+        alt="A 3D model of an astronaut"
+        shadow-intensity="1"
+        camera-controls
+        auto-rotate
+        ar
+      ></model-viewer>
+    </div>
+  );
+};
+
+export default Model;
